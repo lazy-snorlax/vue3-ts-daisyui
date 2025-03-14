fc1d6bc2647b1e32d9dd9f273e44f4bcebe1fee1
Anthony <blacknebula236@gmail.com>
2025-03-14 14:49:34 +1030

    remove HelloWord component & unneeded styles & assets

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div class="hero bg-base-200 min-h-screen">
     <div class="hero-content text-center">
@@ -16,17 +12,6 @@ import HelloWorld from './components/HelloWorld.vue'
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup lang="ts">
+
+</script>
